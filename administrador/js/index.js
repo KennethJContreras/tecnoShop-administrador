7ff3f434ed8b1c8ab591bca74a12be41bf1f4cd4
KennethJContreras <126706972+KennethJContreras@users.noreply.github.com>
2023-04-02 22:42:56 -0600

    Add files via upload

---
+++ administrador/js/index.js
@@ -0,0 +1,8 @@
+var ordenesContainer = document.getElementById(`ordenes-container`);
+
+const ordenesDisponibles = require(`../data/ordenesDisponibles.json`);
+
+console.log(ordenesDisponibles);
+
+console.log(`holap`);
+
